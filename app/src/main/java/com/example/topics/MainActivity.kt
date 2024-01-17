ce76014100be4bc172573672281f2a1a93d7c9c3
JeremyZA-tech <jeremyzamalloaarmas@gmail.com>
2024-01-17 10:49:17 +0100

    Initial commit

--- app/src/main/java/com/example/topics/MainActivity.kt
+++ app/src/main/java/com/example/topics/MainActivity.kt
@@ -6,19 +6,27 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
 import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.integerResource
@@ -26,6 +34,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.topics.data.DataSource
 import com.example.topics.model.Topic
 import com.example.topics.ui.theme.TopicsTheme
 
@@ -48,12 +57,28 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun TopicsApp(){
+    TopicList(
+        topicList = DataSource.topics
+    )
+}
 
+@Composable
+fun TopicList(topicList: List<Topic>, modifier: Modifier = Modifier ){
+    LazyColumn(modifier = modifier
+        .fillMaxWidth()){
+        items(topicList){ topic ->
+            TopicCard(
+                topic =topic,
+                modifier = Modifier.padding(8.dp)
+                )
+        }
+    }
 }
 
 @Composable
 fun TopicCard(topic: Topic, modifier: Modifier = Modifier){
-    Card {
+    Card (modifier = Modifier
+        .fillMaxWidth()){
 
         Row {
             Image(
@@ -62,21 +87,33 @@ fun TopicCard(topic: Topic, modifier: Modifier = Modifier){
                 modifier = Modifier
                     .size(68.dp)
             )
-            Column {
+            Column  {
                 Text(
                     text = LocalContext.current.getString(topic.stringResourceId),
-                    modifier = Modifier.padding(16.dp),
+                    modifier = Modifier.padding(
+                        start = 16.dp,
+                        top = 16.dp,
+                        end = 16.dp,
+                        bottom = 8.dp
+                        ),
                     style = MaterialTheme.typography.bodyMedium
                 )
                 Row {
                     Image(
                         painter = painterResource(R.drawable.ic_grain),
                         contentDescription = null,
-
-                        )
+                        modifier = Modifier
+                            .padding(
+                                start = 16.dp
+                            ),
+                        colorFilter = ColorFilter.tint(Color.Black)
+                    )
                     Text(
                         text = topic.numTopics.toString(),
-                        modifier = Modifier.padding(16.dp),
+                        modifier = Modifier
+                            .padding(
+                                start = 8.dp
+                            ),
                         style = MaterialTheme.typography.labelMedium
                     )
                 }
